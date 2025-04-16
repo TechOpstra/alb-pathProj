5a0b80440417d1942cde039372330e83a2f36a03
Harish <107391575+8555936327@users.noreply.github.com>
2025-04-16 12:38:23 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "techopsbucket123"
-    key            = "vpcec2/terraform.tfstate3"
+    key            = "vpcec2/terraform.tfstate4"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "demo_table123"
